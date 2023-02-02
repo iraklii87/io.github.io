341ef496369aa40c14350fd50f3e69785f76e3e5
Pr1zrak1 <90926114+iraklii87@users.noreply.github.com>
2023-02-02 19:43:49 +0400

    Initial Commit

---
+++ data.js
@@ -0,0 +1,23 @@
+const development = [
+    {
+        title: "Goldstar-motors.ru | Car Dealing Website",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "assets/img/work/1work.png",
+        url: "https://goldstarmotors.ru/",
+        urltext: "www.goldstarmotors.ru",
+    },
+    {
+        title: "Eagles.com",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "assets/img/work/3work.png",
+        url: "http://carseagle.com/",
+        urltext: "www.carseagle.com",
+    },
+    {
+        title: "Carwin | Car Dealing Website",
+        desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam consectetur impedit non ea corporis! Doloribus at qui perspiciatis voluptate excepturi?",
+        img: "assets/img/work/2work.png",
+        url: "https://carwin.mypanel.ge/",
+        urltext: "www.carwin.ru",
+    },
+]
