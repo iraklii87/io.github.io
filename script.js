a6823f7118f5c945206b5616e919733a41ba0d86
Pr1zrak1 <90926114+iraklii87@users.noreply.github.com>
2023-03-01 20:43:46 +0400

    Add Projects

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 
-// development.forEach(function(value){
-//   $('.about').after(`
-//   <section class=" item">
-//   <div class="container">
-//       <div class="row">
-//           <div class="col-lg-8">
-//               <div class="ratio ratio-4x3">
-//                   <img src="${value.img}" alt="">
-//               </div>
-//           </div>
+development.forEach(function(value){
+  $('.about').after(`
+  <section class=" item">
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-8">
+              <div class="ratio ratio-4x3">
+                  <img src="img/shots/${value.img}" alt="">
+              </div>
+          </div>
 
-//           <div class="col-lg-4">
-//               <h1>${value.title}</h1>
-//               <p>${value.desc}</p>
+          <div class="col-lg-4">
+              <h1>${value.title}</h1>
+              <p>${value.desc}</p>
 
-//               <a href="${value.url}"><img src="img/open_in_new.svg" alt="">${value.urltext}</a>
-//           </div>
-//       </div>
-//   </div>
-// </section>
-//   `)
-// });
+              <a href="${value.url}"><img src="img/open_in_new.svg" alt="">${value.urltext}</a>
+          </div>
+      </div>
+  </div>
+</section>
+  `)
+});
